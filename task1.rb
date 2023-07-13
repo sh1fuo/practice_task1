7bf1ad5a033e0c01b0041a92c20f2c3ef5f21249
sh1fuo <139454715+sh1fuo@users.noreply.github.com>
2023-07-13 13:10:39 +0300

    Add files via upload

---
+++ task1.rb
@@ -0,0 +1,23 @@
+﻿module Validation
+    def self.valid_name?(string)
+      !string.nil? && !string.empty? && string.length <= 40 && string.match?(/\A[A-Za-z\-]+\z/)
+    end
+  
+    def self.valid_inn?(string)
+      !string.nil? && !string.empty? && string.match?(/\A[A-Z]{2}\d{10}\z/)
+    end
+  
+    def self.after_date?(date)
+      date && date <= Date.today
+    end
+  end
+  
+  name = "Valentyn"
+  puts Validation.valid_name?(name) # Output: true
+  
+  inn = "AZ1234543456"
+  puts Validation.valid_inn?(inn) # Output: true
+  
+  date = Date.parse("2003-10-05")
+  puts Validation.after_date?(date) # Output: true
+  
